3b6f6e2d627e50b750fb35c1ae8cdd2cc3371be4
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2026-03-22 17:17:37 +0200

    feat(update TypographyH3): update typings

--- components/ui/typography-h3.tsx
+++ components/ui/typography-h3.tsx
@@ -1,4 +1,11 @@
-export function TypographyH3({ children }: { children: React.ReactNode }) {
+import { ReactNode } from "react";
+
+type TypographyH3Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+export function TypographyH3({ children }: TypographyH3Props) {
   return (
     <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
       {children}
