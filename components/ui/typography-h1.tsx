e320ed115beaf6460ac5eaff52a595972dde5a32
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2026-03-22 17:21:29 +0200

    feat(update TypographyH1): update typings

--- components/ui/typography-h1.tsx
+++ components/ui/typography-h1.tsx
@@ -1,4 +1,11 @@
-export function TypographyH1({ children }: { children: React.ReactNode }) {
+import { ReactNode } from "react";
+
+type TypographyH1Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+export function TypographyH1({ children }: TypographyH1Props) {
   return (
     <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
       {children}
